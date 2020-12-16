c48cb67dccb10d0b7f28d5f0f24c487fb8d123ce
Phanor Coll <phanorcoll@gmail.com>
2020-12-15 23:02:06 -0400

    UPDATE 2020

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/phanorcoll/docker-go-neo4j/backend
+
+go 1.15
+
+require github.com/neo4j/neo4j-go-driver/v4 v4.2.0
--- backend/main.go
+++ backend/main.go
@@ -2,292 +2,103 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
 
-	driver "github.com/johnnadratowski/golang-neo4j-bolt-driver"
+	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 )
 
-// MovieResult is the result of moves when searching
+//Neo4jConfiguration holds the configuration for connecting to the DB
+type Neo4jConfiguration struct {
+	URL      string
+	Username string
+	Password string
+	Database string
+}
+
+//MovieResult ...
 type MovieResult struct {
 	Movie `json:"movie"`
 }
 
-// Movie is a movie
+//Movie ...
 type Movie struct {
-	Released int      `json:"released"`
-	Title    string   `json:"title,omitempty"`
-	Tagline  string   `json:"tagline,omitempty"`
-	Cast     []Person `json:"cast,omitempty"`
+	Released int64  `json:"released"`
+	Title    string `json:"title,omitempty"`
 }
 
-// Person is a person in a movie
+//Person will structure the data for the Person records
 type Person struct {
 	Job  string   `json:"job"`
 	Role []string `json:"role"`
 	Name string   `json:"name"`
 }
 
-// D3Response is the graph response
-type D3Response struct {
-	Nodes []Node `json:"nodes"`
-	Links []Link `json:"links"`
+//newDrive is a method for Neo4jConfiguration to return a connection to the DB
+func (nc *Neo4jConfiguration) newDriver() (neo4j.Driver, error) {
+	return neo4j.NewDriver(nc.URL, neo4j.BasicAuth(nc.Username, nc.Password, ""))
 }
 
-// Node is the graph response node
-type Node struct {
-	Title string `json:"title"`
-	Label string `json:"label"`
-}
+//getDataFunc will query the database and return the result as JSON
+func getDataFunc(driver neo4j.Driver) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		session := driver.NewSession(neo4j.SessionConfig{
+			AccessMode:   neo4j.AccessModeRead,
+			DatabaseName: "neo4j",
+		})
+		defer unsafeClose(session)
 
-// Link is the graph response link
-type Link struct {
-	Source int `json:"source"`
-	Target int `json:"target"`
-}
-
-var (
-	neo4jURL = "bolt://neo4j:testing@neo4j:7687"
-)
-
-func interfaceSliceToString(s []interface{}) []string {
-	o := make([]string, len(s))
-	for idx, item := range s {
-		o[idx] = item.(string)
-	}
-	return o
-}
-
-func defaultHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	body, _ := ioutil.ReadFile("public/index.html")
-	w.Write(body)
-}
-
-func searchHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	query := req.URL.Query()["q"][0]
-	cypher := `
-		MATCH
-			(movie:Movie)
-		WHERE
-			movie.title =~ {query}
-		RETURN
-			movie.title as title, movie.tagline as tagline, movie.released as released`
-
-	db, err := driver.NewDriver().OpenNeo(neo4jURL)
-	if err != nil {
-		log.Println("error connecting to neo4j:", err)
-		w.WriteHeader(500)
-		w.Write([]byte("An error occurred connecting to the DB"))
-		return
-	}
-	defer db.Close()
-
-	param := "(?i).*" + query + ".*"
-	data, _, _, err := db.QueryNeoAll(cypher, map[string]interface{}{"query": param})
-	if err != nil {
-		log.Println("error querying search:", err)
-		w.WriteHeader(500)
-		w.Write([]byte("An error occurred querying the DB"))
-		return
-	} else if len(data) == 0 {
-		w.WriteHeader(404)
-		return
-	}
-
-	results := make([]MovieResult, len(data))
-	for idx, row := range data {
-		results[idx] = MovieResult{
-			Movie{
-				Title:    row[0].(string),
-				Tagline:  row[1].(string),
-				Released: int(row[2].(int64)),
-			},
-		}
-	}
-
-	err = json.NewEncoder(w).Encode(results)
-	if err != nil {
-		log.Println("error writing search response:", err)
-		w.WriteHeader(500)
-		w.Write([]byte("An error occurred writing response"))
-	}
-}
-
-func movieHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	query := req.URL.Path[len("/movie/"):]
-	cypher := `
-		MATCH
-			(movie:Movie {title:{title}})
-		OPTIONAL MATCH
-			(movie)<-[r]-(person:Person)
-		WITH
-			movie.title as title,
-			collect({name:person.name, job:head(split(lower(type(r)),'_')), role:r.roles}) as cast
-		LIMIT 1
-		UNWIND cast as c
-		RETURN title, c.name as name, c.job as job, c.role as role`
-
-	db, err := driver.NewDriver().OpenNeo(neo4jURL)
-	if err != nil {
-		log.Println("error connecting to neo4j:", err)
-		w.WriteHeader(500)
-		w.Write([]byte("An error occurred connecting to the DB"))
-		return
-	}
-	defer db.Close()
-
-	data, _, _, err := db.QueryNeoAll(cypher, map[string]interface{}{"title": query})
-	if err != nil {
-		log.Println("error querying movie:", err)
-		w.WriteHeader(500)
-		w.Write([]byte("An error occurred querying the DB"))
-		return
-	} else if len(data) == 0 {
-		w.WriteHeader(404)
-		return
-	}
-
-	movie := Movie{
-		Title: data[0][0].(string),
-		Cast:  make([]Person, len(data)),
-	}
-
-	for idx, row := range data {
-		movie.Cast[idx] = Person{
-			Name: row[1].(string),
-			Job:  row[2].(string),
-		}
-		if row[3] != nil {
-			movie.Cast[idx].Role = interfaceSliceToString(row[3].([]interface{}))
-		}
-	}
-
-	err = json.NewEncoder(w).Encode(movie)
-	if err != nil {
-		log.Println("error writing movie response:", err)
-		w.WriteHeader(500)
-		w.Write([]byte("An error occurred writing response"))
-	}
-}
-
-func graphHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	limits := req.URL.Query()["limit"]
-	limit := 50
-	var err error
-	if len(limits) > 0 {
-		limit, err = strconv.Atoi(limits[0])
+		query := `MATCH (movie:Movie) RETURN movie.title as title, movie.released as released LIMIT $limit`
+		result, err := session.Run(query, map[string]interface{}{"limit": 10})
 		if err != nil {
-			w.WriteHeader(400)
-			w.Write([]byte("Limit must be an integer"))
+			log.Println("Error querying Neo4j", err)
+			return
 		}
-	}
-
-	cypher := `
-		MATCH
-			(m:Movie)<-[:ACTED_IN]-(a:Person)
-		RETURN
-			m.title as movie, collect(a.name) as cast
-		LIMIT
-			{limit}`
-
-	db, err := driver.NewDriver().OpenNeo(neo4jURL)
-	if err != nil {
-		log.Println("error connecting to neo4j:", err)
-		w.WriteHeader(500)
-		w.Write([]byte("An error occurred connecting to the DB"))
-		return
-	}
-	defer db.Close()
-
-	stmt, err := db.PrepareNeo(cypher)
-	if err != nil {
-		log.Println("error preparing graph:", err)
-		w.WriteHeader(500)
-		w.Write([]byte("An error occurred querying the DB"))
-		return
-	}
-	defer stmt.Close()
-
-	rows, err := stmt.QueryNeo(map[string]interface{}{"limit": limit})
-	if err != nil {
-		log.Println("error querying graph:", err)
-		w.WriteHeader(500)
-		w.Write([]byte("An error occurred querying the DB"))
-		return
-	}
-
-	d3Resp := D3Response{}
-	row, _, err := rows.NextNeo()
-	for row != nil && err == nil {
-		title := row[0].(string)
-		actors := interfaceSliceToString(row[1].([]interface{}))
-		d3Resp.Nodes = append(d3Resp.Nodes, Node{Title: title, Label: "movie"})
-		movIdx := len(d3Resp.Nodes) - 1
-		for _, actor := range actors {
-			idx := -1
-			for i, node := range d3Resp.Nodes {
-				if actor == node.Title && node.Label == "actor" {
-					idx = i
-					break
-				}
-			}
-			if idx == -1 {
-				d3Resp.Nodes = append(d3Resp.Nodes, Node{Title: actor, Label: "actor"})
-				d3Resp.Links = append(d3Resp.Links, Link{Source: len(d3Resp.Nodes) - 1, Target: movIdx})
-			} else {
-				d3Resp.Links = append(d3Resp.Links, Link{Source: idx, Target: movIdx})
-			}
+		var movieResults []MovieResult
+		for result.Next() {
+			record := result.Record()
+			released, _ := record.Get("released")
+			title, _ := record.Get("title")
+			movieResults = append(movieResults, MovieResult{Movie{
+				Released: released.(int64),
+				Title:    title.(string),
+			}})
 		}
-		row, _, err = rows.NextNeo()
-	}
 
-	if err != nil && err != io.EOF {
-		log.Println("error querying graph:", err)
-		w.WriteHeader(500)
-		w.Write([]byte("An error occurred querying the DB"))
-		return
-	} else if len(d3Resp.Nodes) == 0 {
-		w.WriteHeader(404)
-		return
-	}
+		moviesJSON, err := json.Marshal(movieResults)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(moviesJSON)
 
-	err = json.NewEncoder(w).Encode(d3Resp)
-	if err != nil {
-		log.Println("error writing graph response:", err)
-		w.WriteHeader(500)
-		w.Write([]byte("An error occurred writing response"))
-	}
-}
-
-func init() {
-	if os.Getenv("NEO4J_URL") != "" {
-		neo4jURL = os.Getenv("NEO4J_URL")
 	}
 }
 
 func main() {
-	serveMux := http.NewServeMux()
-	serveMux.HandleFunc("/", defaultHandler)
-	serveMux.HandleFunc("/search", searchHandler)
-	serveMux.HandleFunc("/movie/", movieHandler)
-	serveMux.HandleFunc("/graph", graphHandler)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+	configuration := parseConfiguration()
+	driver, err := configuration.newDriver()
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer unsafeClose(driver)
+	http.HandleFunc("/", getDataFunc(driver))
+	http.ListenAndServe(":8080", nil)
+}
 
-	log.Printf("Starting server on port %s with neo4j %s", port, neo4jURL)
-	panic(http.ListenAndServe(":"+port, serveMux))
+func parseConfiguration() *Neo4jConfiguration {
+	return &Neo4jConfiguration{
+		URL:      "neo4j://neo4j:7687",
+		Username: "neo4j",
+		Password: "testing",
+	}
+}
+
+func unsafeClose(closeable io.Closer) {
+	if err := closeable.Close(); err != nil {
+		log.Fatal(fmt.Errorf("could not close resource: %w", err))
+	}
 }
